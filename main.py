cdc737dda96b975528e6c09092f801826ba99dd6
sergey.dmitriev <info1981@yandex.ru>
2022-01-20 22:35:37 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import sqlalchemy   # работа с SQL https://habr.com/ru/post/513328/
+
+
+app = FastAPI()
+# app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/items/{item_id}')
+def items(item_id: int, q: Optional[str] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.put('/item/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'name': item.name, 'price': item.price, 'item_id': item_id, 'is': item.is_offer}
